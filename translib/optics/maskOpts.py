926ea6a73725ce04077aabf149a8d0bb60aa19ed
RazoB4B <luis.a.razo@hotmail.com>
2026-01-16 15:06:51 +0100

    First version used for experimental  data analysis in optics/maskOpts.py

--- translib/optics/maskOpts.py
+++ translib/optics/maskOpts.py
@@ -671,3 +671,147 @@ def FindBigDiffusera0(Input, deph=0, Div=2, LR_init=1, NPad=10, RMax=None, DiffS
 
     Diff = np.exp(1j*Diff)
     return Diff, TotLoss, ElapTime, EnergyLoss
+
+
+def FindDiffuser(Input, deph=0, LR_init=1, NPad=10, TryR=False, InitDiff=None, RMax=None, DiffSize=None, MaxSteps=None, MaxLoss=None):
+    device = "cpu"
+    EarlyS = EarlyStopping(Patience=100, Mindelta=0)
+
+    # Recieving and computing numpy arrays
+    alpha = np.pi
+    Nangle = (2*np.pi)/(alpha)
+    cm1 = np.abs(np.sinc(1/Nangle))/(Nangle-1)*np.exp(-1j*deph)
+    cp1 = np.abs(np.sinc(1/Nangle))/(Nangle-1)*np.exp(1j*deph)
+
+    a02 = Input[0]
+    S0 = Input[1]
+    S1 = Input[2]
+
+    if DiffSize is None:
+        DiffSize = a02.shape[0]
+
+    if MaxLoss is None:
+        MaxLoss = 0.3
+    _MaxLoss = MaxLoss
+    _Counter = 0
+
+    VMasks = VortexMask(DiffSize, 1, Max=RMax)
+
+    #Loading to torch
+    a02_target = torch.from_numpy(a02).to(torch.complex64).to(device)
+    S0_target = torch.from_numpy(S0).to(torch.complex64).to(device)
+    S1_target = torch.from_numpy(S1).to(torch.complex64).to(device)
+
+    a02_target = a02_target/torch.mean(torch.abs(a02_target))
+    S0_target = S0_target/torch.mean(torch.abs(S0_target))
+    S1_target = S1_target/torch.mean(torch.abs(S1_target))
+
+    V0 = torch.from_numpy(VMasks[1]).to(torch.complex64).to(device)
+    Vp1 = torch.from_numpy(VMasks[2]).to(torch.complex64).to(device)
+    Vm1 = torch.from_numpy(VMasks[0]).to(torch.complex64).to(device)
+
+    if InitDiff is None:
+        param_diff = 2*np.pi*(torch.rand(DiffSize, DiffSize, requires_grad=True) - 0.5)
+        param_diff = param_diff.clone().detach().requires_grad_(True)
+        optimizer = torch.optim.Adam([param_diff], lr=LR_init)
+    else:
+        param_diff = torch.from_numpy(InitDiff).clone().detach().to(torch.float32).to(device).requires_grad_(True)
+        optimizer = torch.optim.Adam([param_diff], lr=LR_init)
+
+    if MaxSteps is None:
+        MaxSteps = int(1e5)
+
+    # Optimization
+    TotLoss = np.zeros([MaxSteps])
+    ElapTime = np.zeros([MaxSteps])
+
+    timei = time.time()
+    for _step in tqdm(range(MaxSteps)):
+        optimizer.zero_grad()
+
+        diff = torch.exp(1j*param_diff)
+
+        pred_a0 = GetFarField_Torch(diff*V0, NPad, a02.shape[0]/DiffSize)
+        pred_ap1 = GetFarField_Torch(diff*Vp1, NPad, a02.shape[0]/DiffSize)
+        pred_am1 = GetFarField_Torch(diff*Vm1, NPad, a02.shape[0]/DiffSize)
+
+        pred_a0 = pred_a0/torch.mean(torch.abs(pred_a0))
+        pred_ap1 = pred_ap1*cp1/torch.mean(torch.abs(pred_ap1))
+        pred_am1 = pred_am1*cm1/torch.mean(torch.abs(pred_am1))
+
+        S0_pred = torch.abs(pred_a0)**2 + torch.abs(pred_am1)**2 + torch.abs(pred_ap1)**2
+        S1_pred = pred_a0*pred_am1.conj() + pred_ap1*pred_a0.conj()
+
+        a02_pred = torch.abs(pred_a0)**2/torch.mean(torch.abs(pred_a0)**2)
+        S0_pred = S0_pred/torch.mean(torch.abs(S0_pred))
+        S1_pred = S1_pred/torch.mean(torch.abs(S1_pred))
+
+        loss_a0 = fun.L2_Norm_Torch(a02_pred, a02_target)
+        loss_S0 = fun.L2_Norm_Torch(S0_pred, S0_target)
+        loss_S1 = fun.L2_Norm_Torch(S1_pred, S1_target)
+
+        loss_total = loss_a0 + loss_S0 + loss_S1
+
+        loss_total.backward()
+        optimizer.step()
+
+        TotLoss[_step] = loss_total.item()
+        ElapTime[_step] = time.time() - timei
+
+        EarlyS(loss_total.item())
+        if TryR and (EarlyS.should_stop and loss_total.item()>_MaxLoss):
+            print('Restarting', _Counter+1, loss_total.item())
+            with torch.no_grad():
+                param_diff = 2*np.pi*(torch.rand(DiffSize, DiffSize) - 0.5)
+            param_diff = param_diff.clone().detach().requires_grad_(True)
+            optimizer = torch.optim.Adam([param_diff], lr=LR_init)
+            EarlyS = EarlyStopping(Patience=100, Mindelta=0)
+            _Counter += 1
+            if _Counter>5:
+                _MaxLoss = _MaxLoss+0.05
+                _Counter = 0
+                print('Increasing maximum loss', _MaxLoss)
+        if EarlyS.should_stop:
+            print('Found', loss_total.item())
+            TotLoss = TotLoss[:_step+1]
+            ElapTime = ElapTime[:_step+1]
+            break
+
+    Diff = param_diff.detach().numpy()
+    return Diff, TotLoss, ElapTime
+
+
+def FindBigDiffuser(Input, deph=0, Div=2, LR_init=1, NPad=10, RMax=None, DiffSize=None, MaxSteps=None, MaxLoss=None):
+    a02= Input[0]
+    S0 = Input[1]
+    S1 = Input[2]
+
+    if DiffSize is None:
+        DiffSize = a02.shape[0]
+
+    if MaxLoss is None:
+        MaxLoss = 0.3
+
+    for i in np.flip(range(Div+1)):
+        _a02 = CropCenter(a02, DiffSize//(2**i))
+        _S0 = CropCenter(S0, DiffSize//(2**i))
+        _S1 = CropCenter(S1, DiffSize//(2**i))
+
+        if i == Div:
+            Diff, _TotLoss, _ElapTime = FindDiffuser([_a02, _S0, _S1], deph, LR_init, NPad, True, RMax=RMax, DiffSize=DiffSize//(2**Div), MaxSteps=MaxSteps, MaxLoss=MaxLoss)
+        else:
+            Diff, _TotLoss, _ElapTime = FindDiffuser([_a02, _S0, _S1], deph, LR_init/2, NPad, False, InitDiff=Diff, RMax=RMax, MaxSteps=MaxSteps, MaxLoss=MaxLoss)
+
+        if i != 0:
+            Diff = np.repeat(np.repeat(Diff, 2, axis=0), 2, axis=1)
+
+        try:
+            TotLoss = np.append(TotLoss, _TotLoss)
+            ElapTime = np.append(ElapTime, _ElapTime+ElapTime[-1])
+        except:
+            TotLoss = _TotLoss
+            ElapTime = _ElapTime
+
+    Diff = np.exp(1j*Diff)
+    return Diff, TotLoss, ElapTime
+
