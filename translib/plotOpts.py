fc61dc9bf78eb32f131ff80645bab2188c184e4b
RazoB4B <luis.a.razo@hotmail.com>
2026-02-25 14:19:07 +0100

    A new function has been add to plotOps.py

--- translib/plotOpts.py
+++ translib/plotOpts.py
@@ -9,6 +9,7 @@ Created on Tue Nov  4 12:00:26 2025
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 from colorsys import hls_to_rgb
 from matplotlib.colors import LinearSegmentedColormap
 
@@ -97,3 +98,39 @@ def Colorize(array, theme="dark", saturation=1.0, beta=1.4, transparent=False,
         return np.concatenate([c, alpha_channel], axis=-1)
     else:
         return c
+    
+def CenterProfile(Data, save=False, FigSize=None):
+    if FigSize is None:
+        FigSize = (5,5)
+    Spec = gridspec.GridSpec(ncols=2, nrows=2, wspace=0.05, hspace=0.05,
+                             width_ratios=(1, 0.4), height_ratios=(0.4, 1))
+    
+    Data = Data/np.max(np.abs(Data))
+    x_data = Data[len(Data)//2, :]
+    y_data = Data[:, len(Data)//2]
+    
+    fig = plt.figure(figsize=FigSize)
+    ax = []
+    ax.append(fig.add_subplot(Spec[2]))
+    ax.append(fig.add_subplot(Spec[0]))
+    ax.append(fig.add_subplot(Spec[3]))
+    
+    ax[0].imshow(np.abs(Data), cmap=ColorMaps('BlackBlue'))
+    ax[0].axhline(len(x_data)//2, color='C1', ls='--', alpha=0.6)
+    ax[0].axvline(len(x_data)//2, color='C2', ls='--', alpha=0.6)
+    
+    ax[1].plot(np.arange(len(x_data))-len(x_data)//2, np.abs(x_data), color='C1')
+    ax[2].plot(np.abs(y_data), np.arange(len(y_data))-len(y_data)//2, color='C2')
+    
+    ax[0].axis('off')
+    ax[1].set_xlim(-len(x_data)//2, len(x_data)//2)
+    ax[1].set_ylim(0,1) 
+    ax[1].xaxis.tick_top()
+    ax[1].xaxis.set_label_position("top")
+    ax[2].set_ylim(-len(y_data)//2, len(y_data)//2)
+    ax[2].set_xlim(0,1)
+    ax[2].yaxis.tick_right()
+    ax[2].yaxis.set_label_position("right")
+    if save==True:
+        plt.savefig('Fig.svg', bbox_inches='tight', pad_inches=0.01)
+    plt.show()
