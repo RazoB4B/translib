61892ae35d0e7846c60ce7d092d0b35481429bed
RazoB4B <luis.a.razo@hotmail.com>
2026-03-05 17:03:49 +0100

    Modifications in optics/phaseRet.py

--- translib/optics/phaseRet.py
+++ translib/optics/phaseRet.py
@@ -14,15 +14,27 @@ from scipy.ndimage import map_coordinates
 
 
 def VortexMask(size, ordmax=1, Sym=True, Max=None, Min=None):
-    """
+    '''
     Generates spiral phase mask exp(i n phi)
 
-    Size: Size of the array side
-    ordmax: Maximum order of the mask
-    Sym: if True, generate mask considering [-n, n]
-    Max: Cuts the values with r>Max
-    Min: Cuts the values with r<Min
-    """
+    Parameters
+    ----------
+    Size : Integer
+           Size of the array side
+    ordmax : Integer
+             Maximum order of the mask
+    Sym : Boolean
+          If True, generate mask considering [-n, n]
+    Max : Float in [0, 2]
+          Cuts the values with r>Max
+    Min : Float in [0, 2]
+          Cuts the values with r<Min
+
+    Returns
+    -------
+    2D float : A stack of 2D arrays with an amplitude step function 
+               defined by Min and Max, and with phases exp(i n phi)
+    '''
     if Max is None:
         Max = 2
     if Min is None:
@@ -54,14 +66,25 @@ def VortexMask(size, ordmax=1, Sym=True, Max=None, Min=None):
 
 
 def GetFarField(array, Npad=5, WinSize=None, Scale=None):
-    """
+    '''
     Computes the far field propagation of a given array
 
-    array: the array
-    Npad: the size of the padded figure used to compute the Fourier transform
-    WinSize: the size of the final figure
-    Scale: Scales that resample the image
-    """
+    Parameters
+    ----------
+    array : 2D complex float array
+            The array to propagate
+    NPad : Float
+           The size of the padded (N*len(array)) figure used to compute 
+           the Fourier transform
+    WinSize : Integer
+              the size of the final figure
+    Scale : Float
+            Scales used to resample the image
+
+    Returns
+    -------
+    2D float : A 2D array given as the Fourier transform of array
+    '''
     n = len(array)
     if WinSize is None:
         WinSize = n
@@ -75,14 +98,25 @@ def GetFarField(array, Npad=5, WinSize=None, Scale=None):
 
 
 def GetFarField_Torch(array, Npad=5, WinSize=None, Scale=None):
-    """
+    '''
     Computes the far field propagation of a given tensor
 
-    array: the array
-    Npad: the size of the padded figure used to compute the Fourier transform
-    WinSize: the size of the final figure
-    Scale: Scales that resample the image
-    """
+    Parameters
+    ----------
+    array : 2D complex float tensor
+            The array to propagate
+    NPad : Float
+           The size of the padded (N*len(array)) figure used to compute
+           the Fourier transform
+    WinSize : Integer
+              the size of the final figure
+    Scale : Float
+            Scales used to resample the image
+
+    Returns
+    -------
+    2D float tensor : A 2D tensor given as the Fourier transform of tensor
+    '''
     n = array.shape[-1]
     if WinSize is None:
         WinSize = n
@@ -102,12 +136,21 @@ def GetFarField_Torch(array, Npad=5, WinSize=None, Scale=None):
 
 
 def CropCenter(array, crop):
-    """
+    '''
     Crops the central section of a given array
 
-    array: the array to crop
-    crop: number of pixels size we will extract
-    """
+    Parameters
+    ----------
+    array : 2D complex float array
+            The array to crop
+    crop : Integer
+           Number of pixels size we want extract
+
+    Returns
+    -------
+    2D float array : A 2D array given as the center of the original
+                     one with size crop*crop
+    '''
     x, y = array.shape
     startx = x//2-(crop//2)
     starty = y//2-(crop//2)
@@ -115,12 +158,21 @@ def CropCenter(array, crop):
 
 
 def CropCenter_Torch(tensor, size):
-    """
+    '''
     Crops the central section of a given tensor
 
-    tensor: the tensor to crop
-    crop: number of pixels size we will extract
-    """
+    Parameters
+    ----------
+    tensor : 2D complex float tensor
+             The tensor to crop
+    crop : Integer
+           Number of pixels size we want extract
+
+    Returns
+    -------
+    2D float tensor : A 2D tensor given as the center of the original
+                      one with size crop*crop
+    '''
     center = tensor.shape[-1] // 2
     half = size // 2
 
@@ -129,12 +181,21 @@ def CropCenter_Torch(tensor, size):
 
 
 def Resample(Array, Scale):
-    """
-    Resamples the a given array to certain scale
+    '''
+    Resamples a given 2D array to certain scale
 
-    array: the array to resample
-    scale: the value to rescale the array
-    """
+    Parameters
+    ----------
+    Array : 2D complex float array
+            The array to resample
+    Scale : Integer
+            the value to rescale the array
+
+    Returns
+    -------
+    2D float array : A 2D array of the same size of the original but
+                     stretch according to Scale
+    '''
     H, W = Array.shape
 
     # Build coordinate grid
@@ -160,12 +221,21 @@ def Resample(Array, Scale):
 
 
 def Resample_Torch(tensor, scale):
-    """
-    Resamples the a given tensor to certain scale
+    '''
+    Resamples a given 2D tensor to certain scale
 
-    tensor: the array to resample
-    scale: the value to rescale the tensor
-    """
+    Parameters
+    ----------
+    tensor : 2D complex float tensor
+             The tensor to resample
+    Scale : Integer
+            the value to rescale the tensor
+
+    Returns
+    -------
+    2D float tensor : A 2D tensor of the same size of the original but
+                      stretch according to Scale
+    '''
     device = tensor.device
     H, W = tensor.shape
     y = torch.linspace(-1, 1, H, device=device)
@@ -183,42 +253,100 @@ def Resample_Torch(tensor, scale):
 
     sampled_imag = torch.nn.functional.grid_sample(
         U_imag, scaled_grid.unsqueeze(0), mode='bilinear', align_corners=True)
-
     return sampled_real.squeeze() + 1j * sampled_imag.squeeze()
 
 
+def Norm(tensor):
+    '''
+    Normalize a given 2D tensor to the mean of its amplitude
+
+    Parameters
+    ----------
+    tensor : 2D complex float tensor
+             The tensor to normalize
+
+    Returns
+    -------
+    2D float tensor : A 2D tensor indentical to the original but
+                      normalized
+    '''
+    return tensor/torch.mean(torch.abs(tensor))
+
+
 def L2_Norm(x, y):
-    """
+    '''
     Computes the norm in 2D between two different arrays
-    
-    x: the array 1
-    y: the array 2
-    """
+
+    Parameters
+    ----------
+    x : 2D complex float array
+        The array 1
+    y : 2D complex float array
+        The array 2
+
+    Returns
+    -------
+    Float : The L2 difference between two arrays
+    '''
     return np.sqrt(np.mean(np.abs(x - y)**2))
 
 
 def L2_Norm_Torch(x, y):
-    """
+    '''
     Computes the norm in 2D between two different tensors
-    
-    x: the array 1
-    y: the array 2
-    """
+
+    Parameters
+    ----------
+    x : 2D complex float tensor
+        The tensor 1
+    y : 2D complex float tensor
+        The tensor 2
+
+    Returns
+    -------
+    Float : The L2 difference between two tensors
+    '''
     return torch.sqrt(torch.mean(torch.abs(x - y)**2))
 
 
+def GetFourierConstants(alpha, deph=0):
+    '''
+    Computes the coeffitiens of the fourier serie used in the algorithm
+
+    Parameters
+    ----------
+    alpha : Float
+            The angle of the apperture of the temporal mask
+    deph : Float
+           A dephasing factor that add an acoustic phase to the coeffitiens
+
+    Returns
+    -------
+    2 Floats : The complex coeffitients of the Fourier serie
+    '''
+    Nangle = (2*np.pi)/(alpha)
+    cm1 = np.abs(np.sinc(1/Nangle))/(Nangle-1)*np.exp(-1j*deph)
+    cp1 = np.abs(np.sinc(1/Nangle))/(Nangle-1)*np.exp(1j*deph)
+    return cp1, cm1
+
+
 class EarlyStopping:
-    """
+    '''
     Early stopping function for the torch optimizer
 
-    Initialization: EarlyS = EarlyStopping(Patience=100, Mindelta=0)
-    Usage: EarlyS(Error)
+    Initialization : EarlyS = EarlyStopping(Patience=100, Mindelta=0)
+    Usage : EarlyS(Error)
 
-    Patience: Number of steps after the code stops
-    Mindelta: Minimum value of decreasing of the error to consider the optimizer
-              is not improving
-    Error: Error to minimice by the optimizer
-    """
+    Parameters
+    ----------
+    Patience : Integer
+               Number of steps after the code stops
+    Mindelta : Float
+               Minimum value of decreasing of the error to consider the
+               optimizer is not improving
+    Loss : Float
+           Error to minimice by the optimizer
+    '''
     def __init__(self, Patience=500, Mindelta=0):
         self.Patience = Patience
         self.Mindelta = Mindelta
@@ -237,19 +365,25 @@ class EarlyStopping:
 
 
 class IncreasingThreshold:
-    """
+    '''
     Function for the torch optimizer. Since it is considered a certain maximum of
     error to accept or not a diffuser, and that such maximum can be hardly achieve
     when considering systems with disorder.
 
-    Initialization: Threshold = IncreasingThreshol(Patience=5, Increase=0.05)
-    Usage: MaxLoss = Threshold(MaxLoss, Loss)
-
-    Patience: Number of times the solver will restar before increasing the threshold
-    Increase: Increase of the maximum threshold
-    MaxLoss: Current threshold
-    Loss: Error to minimice by the optimizer 
-    """
+    Initialization : Threshold = IncreasingThreshol(Patience=5, Increase=0.05)
+    Usage : MaxLoss = Threshold(MaxLoss, Loss)
+    
+    Parameters
+    ----------
+    Patience : Integer
+               Number of times the solver will restar before increasing the threshold
+    Increase : Float
+               Increase of the maximum threshold
+    MaxLoss : Float
+              Current threshold
+    Loss : Float
+           Error to minimice by the optimizer 
+    '''
     def __init__(self, Patience=5, Increase=0.05):
         self.Patience = Patience
         self.Increase = Increase
@@ -267,17 +401,23 @@ class IncreasingThreshold:
 
 
 class SavingBest:
-    """
+    '''
     Function for the torch optimizer. Keeps the best diffuser ever found if it is not
     good enough for the current threshold. If threshold becomes larger than the storaged
     diffuser, then this array is automatically taken.
 
-    Initialization: Best = SavingBest()
-    Usage: Best(Diff, Loss)
-
-    Diff: Diffuser
-    Loss: Error to minimice by the optimizer
-    """
+    Initialization : Best = SavingBest()
+    Usage : Best(Diff, Loss, Scale)
+    
+    Parameters
+    ----------
+    Diff : 2D complex float tensor
+           The diffuser
+    Loss : Float
+           Error to minimice by the optimizer
+    Scale : Float
+            The scale obtained for Diff
+    '''
     def __init__(self):
         self.Best = None
         self.Loss = None
@@ -290,17 +430,6 @@ class SavingBest:
             self.Scale = Scale
             print(f'Saving best diffuser with loss {self.Loss} and scale {self.Scale}')
 
-
-def Norm(Array):
-    return Array/torch.mean(torch.abs(Array))
-    
-
-def GetFourierConstants(alpha, deph=0):
-    Nangle = (2*np.pi)/(alpha)
-    cm1 = np.abs(np.sinc(1/Nangle))/(Nangle-1)*np.exp(-1j*deph)
-    cp1 = np.abs(np.sinc(1/Nangle))/(Nangle-1)*np.exp(1j*deph)
-    return cp1, cm1
-            
 
 def FindDiffuser(Input, LR_init=1, LR_prop=0.01, deph=0, TryR=False, InitSca=None,  InitDiff=None, 
                  DiamDiff=None, MaxSteps=None, MaxLoss=None, NPad=None):
@@ -327,7 +456,7 @@ def FindDiffuser(Input, LR_init=1, LR_prop=0.01, deph=0, TryR=False, InitSca=Non
         MaxSteps = int(1e5)
         
     if DiamDiff is None:
-        RMax = 1
+        RMax = 1 
     else:
         RMax = DiamDiff/SizeArray
 
@@ -437,7 +566,7 @@ def FindBigDiffuser(Input, Div=2, LR_init=1, LR_prop=0.01, deph=0, InitSca=None,
                     DiamDiff=_DiamDiff, MaxSteps=MaxSteps, MaxLoss=MaxLoss, NPad=NPad)
         else:
             print(f'Increasing the size of the system to {len(_a02)}x{len(_a02)} pixels')
-            Diff, Scale, _TotLoss, _ElapTime = FindDiffuser([_a02, _S0, _S1], LR_init/2, LR_prop/2, deph, False, InitSca=Scale,
+            Diff, Scale, _TotLoss, _ElapTime = FindDiffuser([_a02, _S0, _S1], LR_init/2, LR_prop, deph, False, InitSca=Scale,
                     InitDiff=Diff, DiamDiff=_DiamDiff, MaxSteps=MaxSteps, MaxLoss=MaxLoss, NPad=NPad)
 
         if i != 0:
@@ -452,153 +581,3 @@ def FindBigDiffuser(Input, Div=2, LR_init=1, LR_prop=0.01, deph=0, InitSca=None,
 
     Diff = np.exp(1j*Diff)
     return Diff, Scale, TotLoss, ElapTime
-
-'''
-def FindDiffusera0(Input, deph=0, LR_init=1, NPad=10, TryR=False, InitDiff=None, RMax=None, DiffSize=None, MaxSteps=None, MaxLoss=None):
-    device = "cpu"
-    EarlyS = EarlyStopping(Patience=100, Mindelta=0)
-    Threshold = IncreasingThreshold(Patience=5, Increase=0.05)
-
-    # Recieving and computing numpy arrays
-    alpha = np.pi
-    Nangle = (2*np.pi)/(alpha)
-    cm1 = np.abs(np.sinc(1/Nangle))/(Nangle-1)*np.exp(-1j*deph)
-    cp1 = np.abs(np.sinc(1/Nangle))/(Nangle-1)*np.exp(1j*deph)
-
-    a02 = Input[0]
-    S0 = Input[1]
-    S1 = Input[2]
-    a0 = Input[3]
-
-    if DiffSize is None:
-        DiffSize = a02.shape[0]
-
-    if MaxLoss is None:
-        MaxLoss = 0.3
-
-    if MaxSteps is None:
-        MaxSteps = int(1e5)
-
-    VMasks = VortexMask(DiffSize, 1, Max=RMax)
-
-    #Loading to torch
-    a02_target = torch.from_numpy(a02).to(torch.complex64).to(device)
-    S0_target = torch.from_numpy(S0).to(torch.complex64).to(device)
-    S1_target = torch.from_numpy(S1).to(torch.complex64).to(device)
-    a0_target = torch.from_numpy(a0).to(torch.complex64).to(device)
-
-    a02_target = a02_target/torch.mean(torch.abs(a02_target))
-    S0_target = S0_target/torch.mean(torch.abs(S0_target))
-    S1_target = S1_target/torch.mean(torch.abs(S1_target))
-    a0_target = a0_target/torch.mean(torch.abs(a0_target))
-
-    V0 = torch.from_numpy(VMasks[1]).to(torch.complex64).to(device)
-    Vp1 = torch.from_numpy(VMasks[2]).to(torch.complex64).to(device)
-    Vm1 = torch.from_numpy(VMasks[0]).to(torch.complex64).to(device)
-
-    if InitDiff is None:
-        param_diff = 2*np.pi*(torch.rand(DiffSize, DiffSize, requires_grad=True) - 0.5)
-        param_diff = param_diff.clone().detach().requires_grad_(True)
-        optimizer = torch.optim.Adam([param_diff], lr=LR_init)
-    else:
-        param_diff = torch.from_numpy(InitDiff).clone().detach().to(torch.float32).to(device).requires_grad_(True)
-        optimizer = torch.optim.Adam([param_diff], lr=LR_init)
-        
-    # Optimization
-    TotLoss = np.zeros([MaxSteps])
-    ElapTime = np.zeros([MaxSteps])
-    EnergyLoss = np.zeros([MaxSteps])
-
-    timei = time.time()
-    for _step in tqdm(range(MaxSteps)):
-        optimizer.zero_grad()
-
-        diff = torch.exp(1j*param_diff)
-
-        pred_a0 = GetFarField_Torch(diff*V0, NPad, a02.shape[0]/DiffSize)
-        pred_ap1 = GetFarField_Torch(diff*Vp1, NPad, a02.shape[0]/DiffSize)
-        pred_am1 = GetFarField_Torch(diff*Vm1, NPad, a02.shape[0]/DiffSize)
-
-        pred_a0 = pred_a0/torch.mean(torch.abs(pred_a0))
-        pred_ap1 = pred_ap1*cp1/torch.mean(torch.abs(pred_ap1))
-        pred_am1 = pred_am1*cm1/torch.mean(torch.abs(pred_am1))
-
-        S0_pred = torch.abs(pred_a0)**2 + torch.abs(pred_am1)**2 + torch.abs(pred_ap1)**2
-        S1_pred = pred_a0*pred_am1.conj() + pred_ap1*pred_a0.conj()
-
-        a02_pred = torch.abs(pred_a0)**2/torch.mean(torch.abs(pred_a0)**2)
-        S0_pred = S0_pred/torch.mean(torch.abs(S0_pred))
-        S1_pred = S1_pred/torch.mean(torch.abs(S1_pred))
-
-        loss_a0 = L2_Norm_Torch(a02_pred, a02_target)
-        loss_S0 = L2_Norm_Torch(S0_pred, S0_target)
-        loss_S1 = L2_Norm_Torch(S1_pred, S1_target)
-
-        loss_total = loss_a0 + loss_S0 + loss_S1
-
-        loss_total.backward()
-        optimizer.step()
-
-        EnergyMap = torch.abs(torch.fft.fft2(torch.exp(1j*(torch.angle(a0_target) - torch.angle(pred_a0)))))
-
-        TotLoss[_step] = loss_total.item()
-        ElapTime[_step] = time.time() - timei
-        EnergyLoss[_step] = (torch.max(EnergyMap)/DiffSize**2).item()
-
-        EarlyS(loss_total.item())
-        if TryR and (EarlyS.should_stop and loss_total.item()>MaxLoss):
-            EarlyS = EarlyStopping(Patience=100, Mindelta=0)
-            MaxLoss = Threshold(MaxLoss, loss_total.item())
-            with torch.no_grad():
-                param_diff = 2*np.pi*(torch.rand(DiffSize, DiffSize) - 0.5)
-            param_diff = param_diff.clone().detach().requires_grad_(True)
-            optimizer = torch.optim.Adam([param_diff], lr=LR_init)
-        if EarlyS.should_stop:
-            print('Found', loss_total.item())
-            TotLoss = TotLoss[:_step+1]
-            ElapTime = ElapTime[:_step+1]
-            EnergyLoss = EnergyLoss[:_step+1]
-            break
-
-    Diff = param_diff.detach().numpy()
-    return Diff, TotLoss, ElapTime, EnergyLoss
-
-
-def FindBigDiffusera0(Input, deph=0, Div=2, LR_init=1, NPad=10, RMax=None, DiffSize=None, MaxSteps=None, MaxLoss=None):
-    a02= Input[0]
-    S0 = Input[1]
-    S1 = Input[2]
-    a0 = Input[3]
-
-    if DiffSize is None:
-        DiffSize = a02.shape[0]
-
-    if MaxLoss is None:
-        MaxLoss = 0.3
-
-    for i in np.flip(range(Div+1)):
-        _a02 = CropCenter(a02, DiffSize//(2**i))
-        _S0 = CropCenter(S0, DiffSize//(2**i))
-        _S1 = CropCenter(S1, DiffSize//(2**i))
-        _a0 = CropCenter(a0, DiffSize//(2**i))
-
-        if i == Div:
-            Diff, _TotLoss, _ElapTime, _EnergyLoss = FindDiffusera0([_a02, _S0, _S1, _a0], deph, LR_init, NPad, True, RMax=RMax, DiffSize=DiffSize//(2**Div), MaxSteps=MaxSteps, MaxLoss=MaxLoss)
-        else:
-            Diff, _TotLoss, _ElapTime, _EnergyLoss = FindDiffusera0([_a02, _S0, _S1, _a0], deph, LR_init/2, NPad, False, InitDiff=Diff, RMax=RMax, MaxSteps=MaxSteps, MaxLoss=MaxLoss)
-
-        if i != 0:
-            Diff = np.repeat(np.repeat(Diff, 2, axis=0), 2, axis=1)
-
-        try:
-            TotLoss = np.append(TotLoss, _TotLoss)
-            ElapTime = np.append(ElapTime, _ElapTime+ElapTime[-1])
-            EnergyLoss = np.append(EnergyLoss, _EnergyLoss)
-        except:
-            TotLoss = _TotLoss
-            ElapTime = _ElapTime
-            EnergyLoss = _EnergyLoss
-
-    Diff = np.exp(1j*Diff)
-    return Diff, TotLoss, ElapTime, EnergyLoss
-'''
